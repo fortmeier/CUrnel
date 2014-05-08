62f8791fcb47d68ad33a66619eb75d79d5f15a11
Dirk Fortmeier <fortmeier@imi.uni-luebeck.de>
2014-05-08 12:09:09 +0200

    more stuff for registration

--- tools/GenerateCodeRegistration.py
+++ tools/GenerateCodeRegistration.py
@@ -7,48 +7,24 @@ from GenerateCudaCode import *
 
 alpha1, alpha2 = symbols('alpha1 alpha2')
 
-R, T = symbols('R D', cls=Function)
+R, T = symbols('R T', cls=Function)
 
-#M = MatrixSymbol('M', 3, 3)
-
-#A = Matrix([[0,0,0],[-0.5, 0, 0.5],[0,0,0]])
-# pprint (d_dx)
-# pprint (d_dy)
-# pprint (d_dxdx)
-# pprint (d_dydy)
-# pprint (d_dxdy)
-# pprint (d_dydx)
-#a = alpha1 * u(x,y).diff(x).diff(x) + alpha2 * u(x,y).diff(x)
-
-# S = || grad u || ^ 2 = du_dx + du_dy
-S = u(x,y).diff(x) + u(x,y).diff(y)
+# S = 0.5 * || grad u || ^ 2 = 0.5 * (du_dx^2 + du_dy^2)
+S = 0.5 * (u(x,y).diff(x)**2 + u(x,y).diff(y)**2)
 
 # D = (R - T)**2
 # for now, only one direction
-D = (R(x,y) - T(x+u(x,y),y))**2
-E = D + alpha1 * S
+D = 0.5 * (R(x,y) - T(x+u(x,y),y))**2
+#D = 0
+J = D + alpha1 * S
+print D.diff(u(x,y).diff(x))
+print "J: "+str(J)
+# Euler-Lagrange Equation gives the needed linear differential operator:
+# 0 = du_dt - ...
+dJ_du = (J.diff(u(x,y).diff(x))).diff(x) + (J.diff(u(x,y).diff(y))).diff(y)
 
-# ??? gradient fuer eine richtung totales differential???
-du_dt = E.diff(x) + E.diff(y)
-#du_dt = alpha1 * lapl(u(x,y))
 
-print du_dt
-K_diff = CreateKernel(du_dt) 
-print K_diff
-#generateKernelAndSource( K_diff, "diffusionKernel", ["float alpha1"])
-
-#print "Linear-elasticity kernel:"
-#mu, lamda = symbols("mu lamda")
-#fx = (mu+lamda)*(dvg([u(x,y),u(x,y)])).diff(x) + mu*lapl(u(x,y))
-#fy = (mu+lamda)*(dvg([u(x,y),u(x,y)])).diff(y) + mu*lapl(u(x,y))
-
-#print fx
-
-#dfx_dx = fx.diff(x)
-#dfx_dy = fx.diff(y)
-
-#print dfx_dx
-#print (du_dt)
-#pprint (CreateKernel(fx,locals()))
-#pprint (CreateKernel(fy,locals()))
+print dJ_du
+K_diff = CreateKernel(dJ_du) 
+pprint (K_diff)
 
